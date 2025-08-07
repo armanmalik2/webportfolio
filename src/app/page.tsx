91ec303d31383e33cef9895f681a3a50eacbb6c2
armanmalik2 <armanmalik3370@gmail.com>
2025-08-07 14:58:41 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,8 @@
 import Home1 from "./Home/page";
 import Header from "./Header/page";
-import About from "./About/About";
 import Skills from "./Skills/page";
 import Experience from "./Experience/page";
 import ContactForm from "./ContactForm/page";
-import GlassmorphicLogin from "./Component/Assets/LogIN-Page/LogIn";
 import ProjectsSection from "./Project/page";
 import About2 from "./About/page";
 
@@ -12,8 +10,6 @@ export default function Home() {
   return (
     <>
     <div>
-      {/* <GlassmorphicLogin /> */}
-      {/* <AboutSection /> */}
       <Header />
       <Home1 />
       <About2 />
